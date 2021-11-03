34fe52b4c760e7d6ab8f734f11cc91d3c431f082
olga <oluciams84@gmail.com>
2021-11-03 17:03:51 -0500

    Adiciona hooks state y funcion para cambiar el state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
-      </div>
-    );
-  }
-}
+function App (){
 
-export default App;
+  const [value, setValue] = useState(0)
+
+  return (
+
+    <div>
+        <span className="value">{value}</span>
+        <button onClick={()=>setValue(value + 1)} id="inc">Incrementa</button>
+      </div>
+  )
+
+} 
+
+export default App
+
+
+// class App extends Component {
+//   render() {    
+
+//     return (
+//       <div>
+//         <span className="value">0</span>
+//         <button id="inc">Incrementa</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
